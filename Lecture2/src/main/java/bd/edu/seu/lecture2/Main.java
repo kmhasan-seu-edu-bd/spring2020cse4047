790d07c1caef96102f3cce934bb41fc6484b5c82
Monirul Hasan <kmhasan@gmail.com>
2020-02-18 16:32:58 +0600

    lecture 4

--- Lecture2/src/main/java/bd/edu/seu/lecture2/Main.java
+++ Lecture2/src/main/java/bd/edu/seu/lecture2/Main.java
@@ -33,10 +33,88 @@ public class Main {
         }
     }
 
+    private double getAverageCgpa(List<Student> studentList) {
+        double sum = 0;
+        for (int i = 0; i < studentList.size(); i++) {
+            sum = sum + studentList.get(i).getCgpa();
+        }
+        double average = sum / studentList.size();
+        return average;
+    }
+
+    private double getAverageCgpaWithEnhancedForLoop(List<Student> studentList) {
+        double sum = 0;
+        for (Student student : studentList) {
+            sum = sum + student.getCgpa();
+        }
+        double average = sum / studentList.size();
+        return average;
+    }
+
+    private double getAverageCgpaWithStream(List<Student> studentList) {
+        return studentList
+                .stream()
+                .mapToDouble(student -> student.getCgpa())
+                .average()
+                .getAsDouble();
+    }
+
+    private double getAverageCgpaWithParallelStream(List<Student> studentList) {
+        return studentList
+                .stream()
+                .parallel()
+                .mapToDouble(student -> student.getCgpa())
+                .average()
+                .getAsDouble();
+    }
+
     public Main() {
         StudentService studentService = new StudentService();
+        long start, stop;
+        start = System.currentTimeMillis();
         List<Student> studentList = studentService.getStudentList();
+        stop = System.currentTimeMillis();
+        System.out.println(studentList.size() + " students");
+        System.out.printf("Time taken %.3f seconds\n", (stop - start) / 1000.0);
 
+        start = System.currentTimeMillis();
+        double avgCgpa1 = getAverageCgpa(studentList);
+        stop = System.currentTimeMillis();
+        System.out.printf("Average CGPA: %.3f\n", avgCgpa1);
+        System.out.printf("Time taken %.3f seconds\n", (stop - start) / 1000.0);
+
+        start = System.currentTimeMillis();
+        double avgCgpa2 = getAverageCgpaWithEnhancedForLoop(studentList);
+        stop = System.currentTimeMillis();
+        System.out.printf("Average CGPA: %.3f\n", avgCgpa2);
+        System.out.printf("Time taken %.3f seconds\n", (stop - start) / 1000.0);
+
+        start = System.currentTimeMillis();
+        double avgCgpa3 = getAverageCgpaWithStream(studentList);
+        stop = System.currentTimeMillis();
+        System.out.printf("Average CGPA: %.3f\n", avgCgpa3);
+        System.out.printf("Time taken %.3f seconds\n", (stop - start) / 1000.0);
+
+        start = System.currentTimeMillis();
+        double avgCgpa4 = getAverageCgpaWithParallelStream(studentList);
+        stop = System.currentTimeMillis();
+        System.out.printf("Average CGPA: %.3f\n", avgCgpa4);
+        System.out.printf("Time taken %.3f seconds\n", (stop - start) / 1000.0);
+
+        start = System.currentTimeMillis();
+        studentList = studentList
+                .stream()
+                .parallel()
+                .sorted((s1, s2) -> {
+                    int cgpa1 = (int) (s1.getCgpa() * 1000);
+                    int cgpa2 = (int) (s2.getCgpa() * 1000);
+                    return cgpa1 - cgpa2;
+                })
+                .collect(Collectors.toList());
+        stop = System.currentTimeMillis();
+        System.out.printf("Time taken for sorting %.3f seconds\n", (stop - start) / 1000.0);
+
+        /*
         System.out.println("Before sorting");
         for (Student student : studentList) {
             System.out.println(student);
@@ -45,18 +123,20 @@ public class Main {
 //        Comparator<Student> studentComparator = new StudentIdComparator();
 //        Comparator<Student> studentComparator = new StudentCgpaComparator();
 //        Comparator<Student> studentComparator = new StudentCgpaComparatorAscending();
-        /*Comparator<Student> studentComparator = new Comparator<Student>() {
-            @Override
-            public int compare(Student a, Student b) {
-                if (a.getCgpa() < b.getCgpa()) {
-                    return -1;
-                } else if (a.getCgpa() > b.getCgpa()) {
-                    return +1;
-                } else {
-                    return 0;
-                }
-            }
-        };*/
+        
+//        Comparator<Student> studentComparator = new Comparator<Student>() {
+//            @Override
+//            public int compare(Student a, Student b) {
+//                if (a.getCgpa() < b.getCgpa()) {
+//                    return -1;
+//                } else if (a.getCgpa() > b.getCgpa()) {
+//                    return +1;
+//                } else {
+//                    return 0;
+//                }
+//            }
+//        };
+
         Comparator<Student> studentComparator = (a, b) -> {
             int inta = (int) (a.getCgpa() * 1000);
             int intb = (int) (b.getCgpa() * 1000);
@@ -92,6 +172,7 @@ public class Main {
                 .mapToDouble(student -> student.getCgpa())
                 .max()
                 .ifPresent(maxCgpa -> System.out.println("Max CGPA " + maxCgpa));
+         */
     }
 
     public static void main(String args[]) {
--- Lecture2/src/main/java/bd/edu/seu/lecture2/StudentService.java
+++ Lecture2/src/main/java/bd/edu/seu/lecture2/StudentService.java
@@ -8,6 +8,8 @@ package bd.edu.seu.lecture2;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -16,20 +18,56 @@ import java.util.List;
 public class StudentService {
     public List<Student> getStudentList() {
         List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student(345, "John Doe", 3.22));
-        studentList.add(new Student(319, "Jane Doe", 3.92));
-        studentList.add(new Student(231, "Salah Uddin", 2.46));
-        studentList.add(new Student(281, "Hasibur Rahman", 1.87));
-        studentList.add(new Student(100, "Abul", 2.11));
-        studentList.add(new Student(101, "Babul", 3.12));
-        studentList.add(new Student(102, "Kabul", 2.99));
-        studentList.add(new Student(103, "Ratul", 0.11));
-        studentList.add(new Student(104, "Putul", 0.87));
-        studentList.add(new Student(105, "Tetul", 3.21));
-        studentList.add(new Student(106, "Tutul", 3.45));
-        
+//        studentList.add(new Student(345, "John Doe", 3.22));
+//        studentList.add(new Student(319, "Jane Doe", 3.92));
+//        studentList.add(new Student(231, "Salah Uddin", 2.46));
+//        studentList.add(new Student(281, "Hasibur Rahman", 1.87));
+//        studentList.add(new Student(100, "Abul", 2.11));
+//        studentList.add(new Student(101, "Babul", 3.12));
+//        studentList.add(new Student(102, "Kabul", 2.99));
+//        studentList.add(new Student(103, "Ratul", 0.11));
+//        studentList.add(new Student(104, "Putul", 0.87));
+//        studentList.add(new Student(105, "Tetul", 3.21));
+//        studentList.add(new Student(106, "Tutul", 3.45));
+        int n = 10_000_000;
+        final double EPS = 1E-4;
+        /*
+        for (int i = 0; i < n; i++) {
+            long id = i + 1;
+            String name = "Mehedi Hasan";
+            double cgpa = Math.random() * 4 + EPS;
+            if (cgpa > 4)
+                cgpa = 4;
+            Student student = new Student(id, name, cgpa);
+            studentList.add(student);
+        }
+        */
+        studentList = IntStream
+                .rangeClosed(1, n)
+                //.parallel()
+                .mapToObj(id -> {
+                    String name = "Mehedi Hasan";
+                    double cgpa = Math.random() * 4 + EPS;
+                    if (cgpa > 4)
+                        cgpa = 4;
+                    Student student = new Student(id, name, cgpa);
+                    return student;
+                })
+                .collect(Collectors.toList());
         //Collections.shuffle(studentList);
         
         return studentList;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
